c730aead825cd68ed972d65da74b38b79d55adb0
shine <26shiine@gmail.com>
2024-08-15 00:42:14 +0700

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger.config';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors();
   setupSwagger(app);
 
   const port = 3000;
